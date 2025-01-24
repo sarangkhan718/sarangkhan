068512362effaa0d719a9a2f51ccd93b16367b1d
sarangkhan718 <sarangkhan718@gmail.com>
2025-01-24 03:39:45 -0800

    heloo

---
+++ index.js
@@ -0,0 +1,61 @@
+// const x = 5;
+// // x = 10;
+// const y = 120;
+// console.log(y);
+
+
+//  document.getElementById("heading").innerHTML = "fsdfs";
+
+// Object
+// let data={
+//     name:"danyal",
+//     fname:"noor",
+//     regno:"1122",
+//     country:"pakistan"
+// }
+// console.log(data.country)
+
+
+// // arrays
+// let a=["noor","ali","anwar","ssss","hshsh","hashim"]
+// console.log(a[4])
+// object 567890-
+let data = {
+    name:"sarang khan",
+    fname:"iqbal",
+    regno:"55555",
+    dateofbirth:"25 july 2003",
+    gender:"male"
+}
+console.log(data.gender)
+// array 
+let a=["sarang","noor","ali","khan","asif","jahan","bashir"]
+console.log(a[6])
+// arthematic operator
+let b=20
+let c=10
+console.log(b+c)
+console.log(b-c)
+console.log(b*c)
+console.log(b/c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ modulas.js
@@ -0,0 +1,150 @@
+// let a =40
+// b = 30
+// console.log(a%b)
+// // increment'''
+// let x=10
+// x ++;
+// let w=x;
+// console.log(w)
+// let b=20
+// b --;
+// let v=b
+// console.log(v)
+// let e=2
+// let t=e**3;
+// console.log(t)
+// let a=20
+// a +=20
+// console.log(a)
+// let sub=20
+// sub -=10
+// console.log(sub)
+// let b="shafiq";
+// b +=" ali"; //b=b+b
+// console.log(`helo ${ b}`)
+// let c="noor"
+// console.log(`how are you ${c} `)
+// let e="sarang khan"
+// console.log(`my name is ${e}`)
+// let aa=30
+// aa +=10
+// console.log(aa)
+// let a=20
+// a +=10
+// console.log(a)
+// let b=30
+// b -=10
+// console.log(b)
+// let c="sarang khan"
+// c += "hasnain"
+// console.log(c)
+// let d="sarang"
+// console.log(`how are you ${d}`)
+// let a="sadaqat ali"
+// console.log(`helo man how are you ${a}`)
+// let b="sarang khan"
+// b +=  " asad"
+// console.log(b)
+// let d=40
+// d +=60
+// console.log(d)
+// let e=100
+// e -= 20
+// console.log(e)
+// let r="this is my name "
+// console.log(`sarang khan ${r}`)
+// let a=200
+// a += 30
+// console.log(a)
+// let o="very beautyful"
+// console.log(`you are ${o}`)
+// let f=4
+// f *=3
+// console.log(f) 
+// comparision operator
+// let a=5
+// let b=2
+// console.log("5!=2",a!=b)
+// logical operators
+// let a=6
+// let b=5
+// let cond1 = a>b;
+// let cond2 = a===b;
+// console.log("cond1 && cond2 =" , cond1 && cond2);
+// conditioanl statemet
+// let age = 16
+// if(age >= 18){
+//     console.log("you can vote")
+// }
+//     if(age < 18){
+//         console.log("you can not vote")
+// }
+// let b=["sarang khan","hasnain","wajahat","shafiq","shafi","ali"]
+// console.log(b[2])
+
+// let a={
+//     name:"sarang",
+//     fname:"ali",
+//     regno:"1234"
+
+
+// console.log(a)
+// let c= "sarangkhan"
+// console.log(`helo ${c}`) 
+
+// let name ="sarang"
+// let lastname="khan"
+// console.log(name+" "+lastname)
+let name = "sarang"
+let lastname="khan"
+console.log(name +" "+lastname);
+// string concat
+
+
+// let sarang="car"
+// let waqar="cat"
+// let fullname=sarang.concat( " "+waqar)
+// console.log(fullname)
+// let sarang="mobile"
+// let waqar="phone"
+// let fullname=sarang.concat(" "+waqar)
+// console.log(fullname);
+let a="ali"
+let b="khan"
+let fullname=a.concat(" "+b)
+console.log(fullname);
+
+
+// template literal
+
+let classs="9"
+let rollno="10"
+let dpt="cs"
+let result=`your class is ${classs} and roll no is ${rollno} study in dpt ${dpt}`
+console.log(result)
+
+
+
+let age=14;
+if (age >=18){
+    console.log("you can vote")
+}
+if(age<18){
+    console.log("you cannot vote");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
